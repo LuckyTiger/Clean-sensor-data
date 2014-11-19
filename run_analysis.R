d0265d2261751eb8e414a9b744582c9ae6dd17fa
LuckyTiger <hustlly@gmail.com>
2014-11-19 14:01:07 +0800

    All finished in Rmd

---
+++ run_analysis.R
@@ -0,0 +1,29 @@
+require(dplyr)
+
+features <- read.table("UCI HAR Dataset/features.txt")
+activity_label <- read.table("UCI HAR Dataset/activity_labels.txt")
+#Get feature names
+train_set <- read.table("UCI HAR Dataset/train/X_train.txt")
+train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train_label <- read.table("UCI HAR Dataset/train/y_train.txt")
+names(train_set) <- features$V2
+names(train_subject)<-"Subject"
+names(train_label) <- "Activity"
+
+#Extract mean and standart deviation for each measurement 
+train_set_part <- train_set[,grepl("mean|std",names(train_set))]
+train_data <- cbind(train_subject,train_label,train_set_part)
+
+test_set <- read.table("UCI HAR Dataset/test/X_test.txt")
+test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test_label <- read.table("UCI HAR Dataset/test/y_test.txt")
+names(test_set) <- features$V2
+names(test_subject)<-"Subject"
+names(test_label) <- "Activity"
+test_set_part <- test_set[,grepl("mean|std",names(test_set))]
+test_data <- cbind(test_subject,test_label,test_set_part)
+
+#Bind train data and test data together
+data <- rbind(train_data,test_data)
+data$Activity <- activity_label[data$Activity,][,2]
+write.table(data, "tidydata.txt")
